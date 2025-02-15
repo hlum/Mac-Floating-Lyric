5499a142d6322b9f64d605ff533e6d6709a115d9
アウン <hlumaungphyo@gmail.com>
2025-02-15 16:10:36 +0900

    created logger

--- Lyrics/Helpers/Logger.swift
+++ Lyrics/Helpers/Logger.swift
@@ -4,5 +4,16 @@
 //
 //  Created by Hlwan Aung Phyo on 2025/02/15.
 //
-
 import Foundation
+import OSLog
+
+public enum Logger {
+    public static let standard: os.Logger = .init(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: LogCategory.apiCall.rawValue
+    )
+}
+
+private enum LogCategory: String {
+    case apiCall = "API Call"
+}
