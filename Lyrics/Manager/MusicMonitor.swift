8f56cc5cd272e19ce9520aafd4ccfbe06c1296f0
アウン <hlumaungphyo@gmail.com>
2025-02-15 17:54:36 +0900

    created musicMonitor to check if the music app is playing music

--- Lyrics/Manager/MusicMonitor.swift
+++ Lyrics/Manager/MusicMonitor.swift
@@ -6,14 +6,16 @@
 //
 
 import Foundation
+import AppKit
 
 class MusicMonitor {
     static let shared = MusicMonitor()
-    
-    var getMusicInfo: ((CurrentMusic) -> Void)? = nil
+    var lastTimePlayState: Bool? // Start as nil to force first update
+     var lastTrackTitle: String? // Track the last song to detect song change
+    var getMusicInfo: ((CurrentMusic) -> Void)?
 
     private init() {}
-    
+
     func startMonitoring() {
         let center = DistributedNotificationCenter.default
         center.addObserver(
@@ -23,19 +25,33 @@ class MusicMonitor {
             object: nil
         )
     }
-    
+
+    func stopMonitoring() {
+        let center = DistributedNotificationCenter.default
+        center.removeObserver(self, name: NSNotification.Name("com.apple.iTunes.playerInfo"), object: nil)
+    }
+
     @objc private func handlePlayerInfo(notification: Notification) {
         guard let userInfo = notification.userInfo as? [String: Any] else {
-            Logger.standard.error("!! Can't get userInfo from Notification")
+            print("!! Can't get userInfo from Notification")
             return
         }
-        
+
         let trackTitle = userInfo["Name"] as? String ?? "No Song"
         let artist = userInfo["Artist"] as? String ?? "Unknown Artist"
         let isPlaying = (userInfo["Player State"] as? String == "Playing")
-        
-        let currentMusic = CurrentMusic(isPlaying: isPlaying, songName: trackTitle, artistName: artist)
-        
-        getMusicInfo?(currentMusic)
+
+        // Check if the song has changed or if the player state changed
+        if lastTrackTitle != trackTitle || lastTimePlayState == nil || isPlaying != lastTimePlayState {
+            lastTrackTitle = trackTitle
+            lastTimePlayState = isPlaying
+            
+            let currentMusic = CurrentMusic(
+                isPlaying: isPlaying,
+                songName: trackTitle,
+                artistName: artist
+            )
+            getMusicInfo?(currentMusic)
+        }
     }
 }
