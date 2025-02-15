0dbc9118aa9975c42bb6356a5fd373cbf3ab76e4
アウン <hlumaungphyo@gmail.com>
2025-02-15 13:45:01 +0900

    Initial Commit

---
+++ Lyrics/LyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsApp.swift
+//  Lyrics
+//
+//  Created by Hlwan Aung Phyo on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct LyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
